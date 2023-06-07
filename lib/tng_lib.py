17cd8fdd0425e4d6b3ad9b83a28df06763bfb184
Andrej Obuljen <andrej.obuljen@uzh.ch>
2023-06-07 15:41:28 +0200

    Update tng_lib.py

--- lib/tng_lib.py
+++ lib/tng_lib.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 import numpy as np 
 import matplotlib.pyplot as plt
-from nbodykit.lab import ArrayMesh
+from nbodykit.lab import ArrayMesh, FieldMesh
 from nbodykit.filters import Gaussian
 from nbodykit.source.catalog import UniformCatalog, ArrayCatalog
 from nbodykit import mockmaker
